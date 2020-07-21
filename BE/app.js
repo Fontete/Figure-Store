e98f8defea257173ff77af404142722f5a99b48b
unknown <phongttit99@gmail.com>
2020-07-22 03:01:57 +0700

    Set up cors

--- BE/app.js
+++ BE/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const {
-    connect
-} = require('mongoose')
+const {connect} = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const validator = require('express-validator')
+const cors = require('cors')
 require('dotenv').config()
 
 const userRouter = require('./routes/user')
@@ -16,28 +15,31 @@ const app = express()
 
 //mongodb
 connect(process.env.URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-}).then(() => {
-    console.log("Database is connecting")
-}).catch(err => {
-    console.error('Failed to connect database...', err)
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
 })
+	.then(() => {
+		console.log('Database is connecting')
+	})
+	.catch(err => {
+		console.error('Failed to connect database...', err)
+	})
 
 //middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(validator())
+app.use(cors())
 
-app.use("/users", userRouter)
-app.use("/categories", categoryRouter)
-app.use("/products", productRouter)
+app.use('/users', userRouter)
+app.use('/categories', categoryRouter)
+app.use('/products', productRouter)
 
 const port = process.env.PORT
 
 app.listen(port || 5000, () => {
-    console.log(` Port ${port} is running`)
+	console.log(` Port ${port} is running`)
 })
