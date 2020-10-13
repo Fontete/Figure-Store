5ff688115c52b1d755d22e7a6a24d3bd5501706f
unknown <phongttit99@gmail.com>
2020-10-13 16:56:41 +0700

    Send email to customer when purchase success

--- BE/controllers/payment.js
+++ BE/controllers/payment.js
@@ -1,4 +1,5 @@
 const braintree = require('braintree')
+const nodemailer = require('nodemailer')
 require('dotenv').config
 
 const gateway = braintree.connect({
@@ -30,7 +31,31 @@ exports.purchaseProcess = (req, res) => {
 		(err, result) => {
 			if (err) {
 				res.status(500).json(err)
-			} else res.json(result)
+			} else {
+				const transporter = nodemailer.createTransport({
+					service: 'gmail',
+					auth: {
+						user: 'pipi.esd@gmail.com',
+						pass: 'pp@fpt.com.vn',
+					},
+				})
+				const mailOptions = {
+					from: '"Kaze Figures Store"' + '<' + 'PhongTT' + ' > ',
+					to: req.profile.email,
+					subject: 'Confirmation',
+					text:
+						'Thank you for your payment. You order is in handling process now!',
+				}
+				transporter.sendMail(mailOptions, function (error, info) {
+					if (error) {
+						console.log(error)
+						res.status(400).send('Error.')
+					} else {
+						console.log('Email sent: ' + info.response)
+						res.json(result)
+					}
+				})
+			}
 		},
 	)
 }
